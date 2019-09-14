852b0a932c54c4dbbb8b6602cc9588080963320c
Asad Saeeduddin <masaeedu@gmail.com>
2019-09-14 06:18:00 -0400

    Make it work for the more complicated example

--- src/Coproducts.hs
+++ src/Coproducts.hs
@@ -25,15 +25,19 @@
   , TypeOperators
   , ScopedTypeVariables
   , InstanceSigs
+  , LambdaCase
 #-}
 
 module Coproducts where
 
+import Unsafe.Coerce
 import Data.Proxy
 import GHC.TypeLits
 
 import Control.Category (Category)
 import qualified Control.Category as C
+
+import qualified Data.Bifunctor as B
 
 -- Some general category theory nonsense
 type Hom c = c -> c -> *
@@ -120,17 +124,14 @@ instance Coproduct (->) (Flip I a) (T a)
 instance GFunctor (->) (->) T where
   gfmap = sum_map (Proxy :: Proxy I)
 
-test :: T Int
-test = (* 2) `gfmap` (Foo $ Just 5)
-
 -- Test it out harder
 -- A sum type that we would like to examine in a "deconstructed" form
 data ProxyF t
   where
-  Request :: a' -> (a -> v) -> ProxyF '(a', a, b', b, m, r, v)
-  Respond :: (b' -> (b, v)) -> ProxyF '(a', a, b', b, m, r, v)
-  M       :: m v            -> ProxyF '(a', a, b', b, m, r, v)
-  Pure    :: r              -> ProxyF '(a', a, b', b, m, r, v)
+  Request ::              a' -> (a -> v) -> ProxyF '(a', a, b', b, m, r, v)
+  Respond ::              (b' -> (b, v)) -> ProxyF '(a', a, b', b, m, r, v)
+  M       :: Functor m => m v            -> ProxyF '(a', a, b', b, m, r, v)
+  Pure    ::              r              -> ProxyF '(a', a, b', b, m, r, v)
 
 -- Witness that Proxy is a coproduct of the following types
 type Request a' a          v = (a', a -> v)
@@ -138,11 +139,39 @@ type Respond      b' b     v = b' -> (b, v)
 type M                 m   v = m v
 type Pure                r   = r
 
+type ProxyObject = (*, *, *, *, * -> *, *, *)
+
+data ProxyArrow (t1 :: ProxyObject) (t2 :: ProxyObject)
+  where
+  ProxyArrow ::
+    (Functor m1 => Functor m2) =>
+    (a'1 -> a'2)             ->
+    (a2 -> a1)               ->
+    (b'2 -> b'1)             ->
+    (b1 -> b2)               ->
+    (forall x. m1 x -> m2 x) ->
+    (r1 -> r2)               ->
+    (v1 -> v2)               ->
+    ProxyArrow '(a'1, a1, b'1, b1, m1, r1, v1) '(a'2, a2, b'2, b2, m2, r2, v2)
+
+instance GCategory ProxyArrow
+  where
+  gidentity = unsafeCoerce $ ProxyArrow id id id id id id id
+  (<<<) (ProxyArrow f1 f2 f3 f4 f5 f6 f7) (ProxyArrow g1 g2 g3 g4 g5 g6 g7) = ProxyArrow (f1 . g1) (g2 . f2) (g3 . f3) (f4 . g4) (f5 . g5) (f6 . g6) (f7 . g7)
+
 data Index (c :: Symbol) t where
-  IRequest :: Request a' a v -> Index "Request" '(a', a, b', b, m, r, v)
-  IRespond :: Respond b' b v -> Index "Respond" '(a', a, b', b, m, r, v)
-  IM       :: M m v          -> Index "M"       '(a', a, b', b, m, r, v)
-  IPure    :: Pure r         -> Index "Pure"    '(a', a, b', b, m, r, v)
+  IRequest ::              Request a' a v -> Index "Request" '(a', a, b', b, m, r, v)
+  IRespond ::              Respond b' b v -> Index "Respond" '(a', a, b', b, m, r, v)
+  IM       :: Functor m => M m v          -> Index "M"       '(a', a, b', b, m, r, v)
+  IPure    ::              Pure r         -> Index "Pure"    '(a', a, b', b, m, r, v)
+
+instance GFunctor ProxyArrow (->) (Index c)
+  where
+  gfmap (ProxyArrow f1 f2 f3 f4 f5 f6 f7) = \case
+    (IRequest (a', f)) -> IRequest (f1 a', f7 . f . f2)
+    (IRespond f)       -> IRespond $ B.bimap f4 f7 . f . f3
+    (IM m)             -> IM $ f5 $ fmap f7 $ m
+    (IPure r)          -> IPure $ f6 $ r
 
 instance Coproduct (->) (Flip Index t) (ProxyF t)
   where
@@ -156,6 +185,6 @@ instance Coproduct (->) (Flip Index t) (ProxyF t)
   match m (M mv)         = m $ Flip $ IM       $ mv
   match m (Pure r)       = m $ Flip $ IPure    $ r
 
--- TODO: Create a `ProxArrow t1 t2` indexed by '(a', a, b', b, m, r, v)
--- TODO: Witness `instance GFunctor ProxyArrow (->) (Index c)`
--- TODO: Witness `instance GFunctor ProxyArrow (->) ProxyF`
+instance GFunctor ProxyArrow (->) ProxyF
+  where
+  gfmap = sum_map (Proxy :: Proxy Index)
